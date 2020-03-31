86523c96758e85ae20a336498ac1185650fe7891
srendon <srendon4@cnm.edu>
2020-03-31 13:40:46 -0600

    Created my first milestone.

--- milestone-1.php
+++ milestone-1.php
@@ -13,7 +13,8 @@
 	<ul>
 		<li>To provide the best customer service, comfortability through mobile Beautician's. To be able to allow
 			customers to choose <br> based on reviews, workmanship, quality, professionalism and exceeding all expectations
-			upon customers request.</li>
+			upon customers request.
+		</li>
 		<li>Provide an extended service.</li>
 	</ul>
 	<p><strong>AUDIENCE:</strong></p>
@@ -22,7 +23,8 @@
 	</ul>
 
 	<p><strong>GOAL:</strong></p>
-	<P>To make good impressions, to attract new clients who are seeking stay at home care.<br>To provide the best customer service,
+	<P>To make good impressions, to attract new clients who are seeking stay at home care.<br>To provide the best
+		customer service,
 		and to improve the clients personal appearance and well being.<br>
 		<br>
 		To have great management and qualifications.
@@ -30,39 +32,46 @@
 
 </div>
 <h2>PERSONA</h2>
-<div> <img src="mackenzie.jpg" height="563" width="800" alt="Makenzie Smith"/> </div>
+<div><img src="mackenzie.jpg" height="563" width="800" alt="Makenzie Smith"/></div>
 
 <ul>
-	<li><strong>Name:</strong>Mackenzie Montiel </li>
+	<li><strong>Name:</strong>Mackenzie Montiel</li>
 	<li><strong>Age:</strong> 29</li>
 	<li><strong>Occupation:</strong> Entrepreneur</li>
 	<li><strong>Technology:</strong><br>
-		<em><strong>Phone</strong></em>: Note Samsung Galaxy 9<br> <em><strong>Laptop</strong></em>: Microsoft Surface laptop 3.<br><em><strong>OS</strong></em>: Windows 10 Pro</li>
+		<em><strong>Phone</strong></em>: Note Samsung Galaxy 9<br> <em><strong>Laptop</strong></em>: Microsoft Surface
+		laptop 3.<br><em><strong>OS</strong></em>: Windows 10 Pro
+	</li>
 </ul>
 
 <p><strong>Attitude/Behavior:</strong></p>
 <ul>
-<li>Likes to get her nails and hair done every 3 to 4 weeks, always has to look professional
-	and is always short on time due to her job.</li>
+	<li>Likes to get her nails and hair done every 3 to 4 weeks, always has to look professional
+		and is always short on time due to her job.
+	</li>
 </ul>
 
-<p><strong>Needs/Frustration</strong></p>
+<p><strong>Needs/Frustration:</strong></p>
 <ul>
 	<li>Mackenzie needs to get her nails done and get a hair cut for the weekend, she likes to stay at the comfort
-		of her house when doing these things, so Mackenzie needs a good person to go to her location and provide great customer service.</li>
+		of her house when doing these things, so Mackenzie needs a good person to go to her location and provide great
+		customer service.
+	</li>
 </ul>
 
 <p><strong>Goals:</strong></p>
 <ul>
-	<li>Mackenzie a professional beautician to go to her place and provide great customer service. She always likes great quality materials that will not
-		damage her hair nor nails.</li>
-	<li>As a customer Mackenzie needs a beautician to go to her by requesting it on her phone of computer to beautify her for the weekend. </li>
+	<li>Mackenzie a professional beautician to go to her place and provide great customer service. She always likes great
+		quality materials that will not
+		damage her hair nor nails.
+	</li>
+	<li>As a customer Mackenzie needs a beautician to go to her by requesting it on her phone of computer to beautify her
+		for the weekend.
+	</li>
 </ul>
 
 
-
 </body>
-
 
 
 </html>
